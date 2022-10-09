a2127c320a2ec7068c0615a8dfe5a9ef49775d9e
FARUQOFC <113996402+Faruq51@users.noreply.github.com>
2022-10-09 22:21:58 +0700

    Update config.js

--- system/config.js
+++ system/config.js
@@ -1,5 +1,5 @@
 // Owner number
-global.owner = '6282141544557'
+global.owner = '6285807264974'
 // Owner name
 global.owner_name = 'Customer Service'
 // Maximum upload file size limit (Default : 50 MB)
@@ -15,7 +15,7 @@ global.evaluate_chars = ['=>', '~>', '<', '>', '$']
 // Country code that will be automatically blocked by the system, when sending messages in private chat
 global.blocks = ['91', '92', '212']
 // Put target jid to forward friends story
-global.forwards = '6282141544557@c.us'
+global.forwards = '6285807264974@c.us'
 // Get neoxr apikey by registering at https://api.neoxr.my.id
 global.Api = new (require('./neoxrApi'))(process.env.API_KEY)
 // Get bid and key configuration for autoreply chat ai feature by registering at https://brainshop.ai
